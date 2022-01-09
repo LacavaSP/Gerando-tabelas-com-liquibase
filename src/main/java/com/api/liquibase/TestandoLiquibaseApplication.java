c3136cf6cf4bdadec3f04a6067bbb7b164ae6408
LacavaSP <deitylink38@gmail.com>
2022-01-09 18:44:26 -0300

    commit

---
+++ src/main/java/com/api/liquibase/TestandoLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.api.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestandoLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestandoLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/liquibase/TestandoLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestandoLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
